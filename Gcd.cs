da9f5ae8bb5038514a6d422c18fdbd4df2aab9c6
Егор Усачёв <egichey@gmail.com>
2020-07-11 14:54:37 +0200

    comments added

--- Gcd.cs
+++ Gcd.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 
 namespace TaskOne
 {
-
+    /// <summary>
+    /// This class is used to calculate the gcd
+    /// </summary>
     public class Gcd
     {
-
+        /// <summary>
+        /// The method uses the Euclidean algorithm to calculate the GCD of 
+        /// 2 integer numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         public virtual int GetGcd(int num1, int num2)
         {
             while (num2 != 0)
@@ -21,22 +29,50 @@ namespace TaskOne
             }
             return num1;
         }
-
+        /// <summary>
+        /// The method uses the Euclidean algorithm to calculate the GCD of 
+        /// 3 integer numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
         public int GetGcd(int num1, int num2, int num3)
         {
             return GetGcd(num3, GetGcd(num1, num2));
         }
-
+        /// <summary>
+        /// The method uses the Euclidean algorithm to calculate the GCD of 
+        /// 4 integer numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
         public int GetGcd(int num1, int num2, int num3, int num4)
         {
             return GetGcd(GetGcd(num1, num2, num3), num4);
         }
-
+        /// <summary>
+        /// The method uses the Euclidean algorithm to calculate the GCD of 
+        /// 5 integer numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
         public int GetGcd(int num1, int num2, int num3, int num4, int num5)
         {
             return GetGcd(GetGcd(num1, num2, num3, num4), num5);
         }
-
+        /// <summary>
+        /// The method uses the Stein's algorithm to calculate the GCD of 
+        /// 2 numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
         public int GetGcdByStein(int num1, int num2, out Stopwatch time)
         {
             time = new Stopwatch();
@@ -66,6 +102,11 @@ namespace TaskOne
             time.Stop();
             return num1;
         }
+        /// <summary>
+        /// Swaps two integer elements.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
         private void Swap(ref int num1, ref int num2)
         {
             int temp = num1;
--- GcdTime.cs
+++ GcdTime.cs
@@ -7,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace TaskOne
 {
+    /// <summary>
+    /// This class is used to measure the runtime of Gcd class methods
+    /// </summary>
     public class GcdTime : Gcd
     {
+        /// <summary>
+        /// Сhanges the basic behavior of the method and uses the 
+        /// subtraction algorithm to find the GCD.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         public override int GetGcd(int num1, int num2)
         {
             if (num1 == 0 || num2 == 0)
@@ -22,6 +32,13 @@ namespace TaskOne
             }
             return num1;
         }
+
+        /// <summary>
+        /// Returns the time taken to complete the GetBaseGcd method.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         public Stopwatch GetBaseGcdTime(int num1, int num2)
         {
             Stopwatch time = new Stopwatch();
@@ -30,6 +47,13 @@ namespace TaskOne
             time.Stop();
             return time;
         }
+
+        /// <summary>
+        /// Returns the time taken to complete the GetGcd method.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         public Stopwatch GetGcdTime(int num1, int num2)
         {
             Stopwatch time = new Stopwatch();
@@ -38,6 +62,14 @@ namespace TaskOne
             time.Stop();
             return time;
         }
+
+        /// <summary>
+        /// Returns a dictionary in which the key is the name of the 
+        /// algorithm, and the value is the time of its execution.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         public Dictionary<string, Stopwatch> GetCalculationTime(int num1, int num2)
         {
             GetGcdByStein(num1, num2, out Stopwatch simpsonTime);

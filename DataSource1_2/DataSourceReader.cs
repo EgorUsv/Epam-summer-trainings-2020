b8eea3581921ef27cc1d3b099997bf9c627e73c0
Егор Усачёв <egichey@gmail.com>
2020-07-11 15:33:20 +0200

    figure handler + data source

---
+++ DataSource1_2/DataSourceReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource1_2
+{
+    public class DataSourceReader
+    {
+        public string Path { get; }
+        public DataSourceReader(string path = "sourceFile.txt")
+        {
+            Path = path;
+        }
+        public bool ReadData(out string result)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path))
+                    result = sr.ReadToEnd();
+                return true;
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+                return false;
+            }
+        }
+    }
+}
---
+++ FigureHandler/FigureHandler.cs
@@ -0,0 +1,63 @@
+﻿using DataSource1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2_1.Figures;
+
+namespace FigureHandler
+{
+    public class FigureHandler
+    {
+        public static Figure InitializeFigure(string name, double[] sides)
+        {
+            if (sides == null || sides.Length == 0)
+                return default;
+            switch (name)
+            {
+                case "triangle":
+                    return new Triangle(sides);
+                case "square":
+                    return new Square(sides.First());
+                case "rectangle":
+                    return new Rectangle(sides);
+                default:
+                    return default;
+            }
+        }
+        public static Figure[] GetArray(DataSourceReader dsr)
+        {
+            if (dsr.ReadData(out string data))
+            {
+                var figureInfo = data.Split('\n');
+                Figure[] figures = InitializeArray(figureInfo);
+                return figures;
+            }
+            else
+                return default;
+
+        }
+        private static Figure[] InitializeArray(string[] figuresInfo)
+        {
+            Figure[] figuresArray = new Figure[figuresInfo.Length];
+            try
+            {
+                int arrIndex = 0;
+                foreach (string info in figuresInfo)
+                {
+                    string[] figure = info.Split(' ');
+                    double[] sides = figure.Skip(1)
+                        .Select(s => double.Parse(s)).ToArray();
+                    figuresArray[arrIndex] = InitializeFigure(figure[0], sides);
+                    arrIndex++;
+                }
+                return figuresArray;
+            }
+            catch (Exception)
+            {
+                return figuresArray;
+            }
+        }
+    }
+}

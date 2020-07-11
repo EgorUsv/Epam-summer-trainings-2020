1246bba13a8537ab8dbecc107c1bc594ce4b7de0
Егор Усачёв <egichey@gmail.com>
2020-07-11 15:40:44 +0200

    added DataSourceTests

---
+++ Task1_2Tests/DataSourceTests/DataSourceTest.cs
@@ -0,0 +1,27 @@
+﻿using DataSource1_2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Task1_2Tests.DataSourceTests
+{
+    public class DataSourceTest
+    {
+        [Fact]
+        public void DataSourceReaderTest()
+        {
+            var expected = "sourceFile.txt";
+            var actual = new DataSourceReader().Path;
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void ReadDataTest()
+        {
+            var path = "..\\..\\..\\DataSourceTests\\sourceFile.txt";
+            var expected = "test text";
+            new DataSourceReader(path).ReadData(out var actual);
+            Assert.Equal(expected, actual);
+        }
+    }
+}

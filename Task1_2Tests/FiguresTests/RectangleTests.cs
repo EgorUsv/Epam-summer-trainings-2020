4690746b25bacb557f2246792813b5fe1de6b9f1
Егор Усачёв <egichey@gmail.com>
2020-07-11 16:34:10 +0200

    added figures tests and findSameFigures

---
+++ Task1_2Tests/FiguresTests/RectangleTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task2_1.Figures;
+using Xunit;
+namespace Task1_2Tests.FiguresTests
+{
+    public class RectangleTests
+    {
+        static double[] TestSides { get; } = { 16.0, 10.0 };
+        static string TestString { get; } = "This is Rectangle";
+        readonly Rectangle testRect = new Rectangle(TestSides);
+
+        [Fact]
+        public void RectangleTest()
+        {
+            var expected = new Rectangle(TestSides);
+            var actual = testRect;
+            Assert.True(Enumerable
+                .SequenceEqual(expected.Sides, actual.Sides));
+        }
+        [Fact]
+        public void PerimeterTest()
+        {
+            var expected = 2 * (TestSides[0] + TestSides[1]);
+            var actual = testRect.Perimeter();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetHashCodeTest()
+        {
+            var actual = new Rectangle(TestSides).GetHashCode();
+            var expected = testRect.GetHashCode();
+            Assert.NotEqual(expected, actual);
+        }
+        [Fact]
+        public void AreaTest()
+        {
+            var expected = TestSides[0] * TestSides[1];
+            var actual = testRect.Area();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EqualsTest()
+        {
+            var expected = new Rectangle(TestSides);
+            var actual = testRect;
+            Assert.True(actual.Equals(expected));
+        }
+        [Fact]
+        public void ToStringTest()
+        {
+            var expected = TestString;
+            var actual = testRect.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Task1_2Tests/FindSameFigures/FindFigures.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task2_1.Figures;
+using Xunit;
+
+namespace Task1_2Tests.FindSameFigures
+{
+    public class FindFigures
+    {
+        [Fact]
+        public void FindIdenticalFigures()
+        {
+            var figures = new List<Figure>(6);
+            figures.Add(new Triangle(new double[] { 3, 4, 5 }));
+            figures.Add(new Square(6));
+            figures.Add(new Rectangle(new double[] { 10, 6 }));
+            figures.Add(new Square(8));
+            figures.Add(new Triangle(new double[] { 14, 16, 17 }));
+            figures.Add(new Rectangle(new double[] { 10, 6 }));
+            var testFigure = new Rectangle(new double[] { 10, 6 });
+            var expected = new List<Figure>
+            {
+                new Rectangle(new double[] { 10, 6 }),
+                new Rectangle(new double[] { 10, 6 })
+            };
+            var actual = figures.FindAll(x => x.Equals(testFigure));
+            Assert.True(Enumerable.SequenceEqual(expected, actual));
+        }
+    }
+}
